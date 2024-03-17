efda719679e1bd439f564695f23695def7425386
Rahul <raht009@gmail.com>
2024-03-17 09:43:58 +0530

    coded'

--- 10-Functions/final/script.js
+++ 10-Functions/final/script.js
@@ -419,8 +419,13 @@ GOOD LUCK 😀
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  anssers: new Array(4).fill(0),
   registerNewAnswer() {
-    const answer = prompt(`${this.question} \n${this.options.join('\n')}`);
+    const answer = Number(
+      prompt(`${this.question} \n${this.options.join('\n')}`)
+    );
+    console.log(answer);
+    typeof answer === Number && answer < length.anssers;
   },
 };
 poll.registerNewAnswer();

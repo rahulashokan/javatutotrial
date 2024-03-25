4757c0774f8c9f985784a34f0bdc4ba3852f4715
Rahul <raht009@gmail.com>
2024-03-26 00:11:32 +0530

    redueDone

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -97,6 +97,9 @@ calcPrintBalance;
 ////////
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+const maxmov = movements.reduce((max, mov) => (max > mov ? max : mov));
+console.log(maxmov);
+
 // const deposits = movements.filter(mov => mov > 0);
 // console.log(movements);
 // console.log(deposits);

d03749a30584567d762d22244df43bcae6228cf4
Rahul <raht009@gmail.com>
2024-05-19 23:58:57 +0530

    changes odne

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const Person = function (name, year) {
+  this.name = name;
+  this.year = year;
+};
+
+const user = new Person('rahul', 1996);
+
+const user1 = new Person('rosmy', 1994);
+
+const user2 = new Person('Akash', 1995);
+
+console.log(user, user1, user2);
+
+Person.prototype.calAge = function () {
+  console.log(2024 - this.year);
+};
+
+user.calAge();
+
+Person.prototype.species = 'Homo sapiens';
+
+console.log(user.__proto__.__proto__);
+
+console.log(Person.prototype.constructor);

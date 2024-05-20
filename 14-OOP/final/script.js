0e6bbc9c0ba2997840c5546c6e79a1f71b4d9265
Rahul <raht009@gmail.com>
2024-05-20 08:21:04 +0530

    1code done

--- 14-OOP/final/script.js
+++ 14-OOP/final/script.js
@@ -100,6 +100,32 @@ DATA CAR 2: 'Mercedes' going at 95 km/h
 GOOD LUCK 😀
 */
 
+const Car = function (make, speed) {
+  (this.make = make), (this.speed = speed);
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`Accelerated to ${this.speed}`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`Braked to ${this.speed}`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.brake();
+bmw.brake();
+mercedes.accelerate();
+
 /*
 const Car = function (make, speed) {
   this.make = make;

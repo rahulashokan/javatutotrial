ac63bb6412474204023530a4bba402d2699831dd
Rahul <raht009@gmail.com>
2024-05-22 00:10:56 +0530

    getter and setter'

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -25,15 +25,24 @@
 
 // console.log(Person.prototype.constructor);
 
-class Person {
-  constructor(firsname, year) {
-    (this.firsname = firsname), (this.year = year);
-  }
-  calAge() {
-    console.log(`Age is ${2024 - this.year}`);
-  }
-}
+// class Person {
+//   constructor(firsname, year) {
+//     (this.firsname = firsname), (this.year = year);
+//   }
+//   calAge() {
+//     console.log(`Age is ${2024 - this.year}`);
+//   }
+// }
 
-const rahul = new Person('Rahul', 1996);
-console.log(rahul);
-rahul.calAge();
+// const rahul = new Person('Rahul', 1996);
+// console.log(rahul);
+// rahul.calAge();
+const account = {
+  name: 'Rahul',
+  movement: [200, 400, 500],
+  get latest() {
+    return this.movement.slice(-1).pop();
+  },
+};
+
+console.log(account.latest);

557be20078aa7eae4c8d1f16b2fc579a91a9348a
Rahul <raht009@gmail.com>
2024-05-24 08:25:59 +0530

    inheritance

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,72 +1,87 @@
 'use strict';
 
-// const Person = function (name, year) {
-//   this.name = name;
-//   this.year = year;
-// };
-
-// const user = new Person('rahul', 1996);
-
-// const user1 = new Person('rosmy', 1994);
-
-// const user2 = new Person('Akash', 1995);
-
-// console.log(user, user1, user2);
-
-// Person.prototype.calAge = function () {
-//   console.log(2024 - this.year);
-// };
-
 // user.calAge();
 
 // Person.prototype.species = 'Homo sapiens';
 
-// console.log(user.__proto__.__proto__);
+// // console.log(user.__proto__.__proto__);
 
-// console.log(Person.prototype.constructor);
+// // console.log(Person.prototype.constructor);
 
-class Person {
-  static date = '1996';
-  constructor(firsname, year) {
-    (this.firsname = firsname), (this.year = year);
-  }
-  calAge() {
-    console.log(`Age is ${2024 - this.year}`);
-  }
+// class Person {
+//   static date = '1996';
+//   constructor(firsname, year) {
+//     (this.firsname = firsname), (this.year = year);
+//   }
+//   calAge() {
+//     console.log(`Age is ${2024 - this.year}`);
+//   }
 
-  static hey() {
-    console.log('Hi there');
-    console.log(this.date);
-  }
-}
+//   static hey() {
+//     console.log('Hi there');
+//     console.log(this.date);
+//   }
+// }
 
-const rahul = new Person('Rahul', 1996);
-console.log(rahul);
-rahul.calAge();
-// rahul.hey();
-Person.hey();
+// const rahul = new Person('Rahul', 1996);
+// console.log(rahul);
+// rahul.calAge();
+// // rahul.hey();
+// Person.hey();
 
-// const account = {
-//   name: 'Rahul',
-//   movement: [200, 400, 500],
-//   get latest() {
-//     return this.movement.slice(-1).pop();
+// // const account = {
+// //   name: 'Rahul',
+// //   movement: [200, 400, 500],
+// //   get latest() {
+// //     return this.movement.slice(-1).pop();
+// //   },
+// // };
+
+// // console.log(account.latest);
+
+// const personProto = {
+//   calAge() {
+//     console.log(2024 - this.birthYear);
 //   },
 // };
 
-// console.log(account.latest);
+// const akash = Object.create(personProto);
+// console.log(akash);
 
-const personProto = {
-  calAge() {
-    console.log(2024 - this.birthYear);
-  },
+// akash.name = 'Akash';
+// akash.birthYear = 1995;
+// akash.calAge();
+
+// ////
+
+const Person = function (name, year) {
+  this.name = name;
+  this.year = year;
 };
 
-const akash = Object.create(personProto);
-console.log(akash);
+Person.prototype.calAge = function () {
+  console.log(2024 - this.year);
+};
 
-akash.name = 'Akash';
-akash.birthYear = 1995;
-akash.calAge();
+const Student = function (name, year, course) {
+  Person.call(this, name, year);
+  this.course = course;
+};
+///linking prototypes
+Student.prototype = Object.create(Person.prototype);
 
-////
+///student prototype
+Student.prototype.introduce = function () {
+  console.log(`Hi i am ${this.name} and i study ${this.course}`);
+};
+
+const rahul = new Student('rahul', 1996, 'Computer science');
+
+console.log(rahul);
+rahul.introduce();
+
+rahul.calAge();
+
+// const user1 = new Person('rosmy', 1994);
+
+// const user2 = new Person('Akash', 1995);

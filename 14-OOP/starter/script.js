0b6905a948f0c3410d6bfaa57cbba67cbc067b5d
Rahul <raht009@gmail.com>
2024-05-26 10:08:12 +0530

    updated the extend

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -54,28 +54,28 @@
 
 // ////
 
-const Person = function (name, year) {
-  this.name = name;
-  this.year = year;
-};
+// const Person = function (name, year) {
+//   this.name = name;
+//   this.year = year;
+// };
 
-Person.prototype.calAge = function () {
-  console.log(2024 - this.year);
-};
+// Person.prototype.calAge = function () {
+//   console.log(2024 - this.year);
+// };
 
-const Student = function (name, year, course) {
-  Person.call(this, name, year);
-  this.course = course;
-};
-///linking prototypes
-Student.prototype = Object.create(Person.prototype);
+// const Student = function (name, year, course) {
+//   Person.call(this, name, year);
+//   this.course = course;
+// };
+// ///linking prototypes
+// Student.prototype = Object.create(Person.prototype);
 
-///student prototype
-Student.prototype.introduce = function () {
-  console.log(`Hi i am ${this.name} and i study ${this.course}`);
-};
+// ///student prototype
+// Student.prototype.introduce = function () {
+//   console.log(`Hi i am ${this.name} and i study ${this.course}`);
+// };
 
-const rahul = new Student('rahul', 1996, 'Computer science');
+// const rahul = new Student('rahul', 1996, 'Computer science');
 
 // console.log(rahul);
 // rahul.introduce();
@@ -86,6 +86,42 @@ const rahul = new Student('rahul', 1996, 'Computer science');
 
 // const user2 = new Person('Akash', 1995);
 
-console.log(Student.prototype);
+// console.log(Student.prototype);
 
-console.log(rahul.__proto__.__proto__);
+// console.log(rahul.__proto__.__proto__);
+
+class Person {
+  static date = '1996';
+
+  constructor(firsname, year) {
+    (this.firsname = firsname), (this.year = year);
+  }
+
+  calAge() {
+    console.log(`Age is ${2024 - this.year}`);
+  }
+
+  static hey() {
+    console.log('Hi there');
+    console.log(this.date);
+  }
+}
+
+class student extends Person {
+  constructor(firsname, year, course) {
+    super(firsname, year);
+    this.course = course;
+  }
+
+  introduce = function () {
+    console.log(`Hi i am ${this.firstname} and i study ${this.course}`);
+  };
+
+  calAge() {
+    console.log(`this is the student ${2024 - this.year}`);
+  }
+}
+
+const rahul = new student('Rahul', 1996, 'comupter science');
+
+rahul.calAge();

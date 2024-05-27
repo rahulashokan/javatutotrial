8eb74ff828a4175da01bb25b4d7f9272432ec2e4
Rahul <raht009@gmail.com>
2024-05-27 20:34:35 +0530

    creatingmethods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -90,68 +90,96 @@
 
 // console.log(rahul.__proto__.__proto__);
 
-class Person {
-  static date = '1996';
+// class Person {
+//   static date = '1996';
 
-  constructor(firsname, year) {
-    (this.firsname = firsname), (this.year = year);
-  }
+//   constructor(firsname, year) {
+//     (this.firsname = firsname), (this.year = year);
+//   }
 
-  calAge() {
-    console.log(`Age is ${2024 - this.year}`);
-  }
+//   calAge() {
+//     console.log(`Age is ${2024 - this.year}`);
+//   }
 
-  static hey() {
-    console.log('Hi there');
-    console.log(this.date);
+//   static hey() {
+//     console.log('Hi there');
+//     console.log(this.date);
+//   }
+// }
+
+// class student extends Person {
+//   constructor(firsname, year, course) {
+//     super(firsname, year);
+//     this.course = course;
+//   }
+
+//   introduce = function () {
+//     console.log(`Hi i am ${this.firstname} and i study ${this.course}`);
+//   };
+
+//   calAge() {
+//     console.log(`this is the student ${2024 - this.year}`);
+//   }
+// }
+
+// const rahul = new student('Rahul', 1996, 'comupter science');
+
+// rahul.calAge();
+
+// ////////////Inheritance between class and Object Create/////
+
+// const Personprototy = {
+//   calcAge() {
+//     console.log(2024 - this.year);
+//   },
+
+//   init(name, year) {
+//     this.name = name;
+//     this.year = year;
+//   },
+// };
+
+// const akash = Object.create(Personprototy);
+// const studentProto = Object.create(Personprototy);
+
+// studentProto.init = function (name, year, course) {
+//   Personprototy.init.call(this, name, year);
+//   this.course = course;
+// };
+
+// studentProto.introduce = function () {
+//   console.log(`Hi i am ${this.name} and i study ${this.course}`);
+// };
+
+// const maya = Object.create(studentProto);
+// maya.init('Maya', 1998, 'Business analysist');
+// maya.introduce();
+// maya.calcAge();
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.local = navigator.language;
+
+    console.log(`Thanks for openning the account ${owner}`);
   }
+  ////public interface of the object
+  deposit(value) {
+    this.movements.push(value);
+  }
+  withdraw(value) {
+    this.deposit(-value);
+  }
+  /////////////////
 }
 
-class student extends Person {
-  constructor(firsname, year, course) {
-    super(firsname, year);
-    this.course = course;
-  }
-
-  introduce = function () {
-    console.log(`Hi i am ${this.firstname} and i study ${this.course}`);
-  };
-
-  calAge() {
-    console.log(`this is the student ${2024 - this.year}`);
-  }
-}
-
-const rahul = new student('Rahul', 1996, 'comupter science');
-
-rahul.calAge();
-
-////////////Inheritance between class and Object Create/////
-
-const Personprototy = {
-  calcAge() {
-    console.log(2024 - this.year);
-  },
-
-  init(name, year) {
-    this.name = name;
-    this.year = year;
-  },
-};
-
-const akash = Object.create(Personprototy);
-const studentProto = Object.create(Personprototy);
-
-studentProto.init = function (name, year, course) {
-  Personprototy.init.call(this, name, year);
-  this.course = course;
-};
-
-studentProto.introduce = function () {
-  console.log(`Hi i am ${this.name} and i study ${this.course}`);
-};
-
-const maya = Object.create(studentProto);
-maya.init('Maya', 1998, 'Business analysist');
-maya.introduce();
-maya.calcAge();
+const acc1 = new Account('Rahul', 'INR', 1111);
+console.log(acc1);
+acc1.deposit(10);
+acc1.withdraw(200);
+// acc1.movements.push(445);
+// acc1.movements.push(-45);
+console.log(acc1);

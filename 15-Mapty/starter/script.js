714b2a6374c56a7f18c726b1430822f0957454b2
Rahul <raht009@gmail.com>
2024-05-30 13:22:28 +0530

    markercompleted

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -30,14 +30,23 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords).addTo(map).bindPopup('My location').openPopup();
-
       map.on('click', function (mapEvent) {
         console.log(mapEvent);
         const { lat, lng } = mapEvent.latlng;
-        L.marker({ lat, lng })
+        // var popup =
+
+        L.marker([lat, lng])
           .addTo(map)
-          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+          .bindPopup(
+            L.popup({
+              maxWidth: 300,
+              minWidth: 50,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Marker added')
           .openPopup();
       });
     },
